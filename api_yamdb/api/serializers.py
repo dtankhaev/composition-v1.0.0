7165724e1d3eb1f7001ba943848a38af8e982d39
Danzan Tankhaev <dtankhaev@gmail.com>
2023-03-19 17:59:11 -0400

    tests4

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,6 @@ import datetime as dt
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.validators import UnicodeUsernameValidator
 
